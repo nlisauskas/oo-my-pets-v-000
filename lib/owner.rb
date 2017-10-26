7e238c5c53b71217066cd762d8f2a15dd90aaebf
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-26 00:54:50 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,17 @@
 class Owner
-  # code goes here
+
+  @@all = []
+
+  def self.all
+    @@all << self
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+
+  def initialize(owner)
+    @owner = owner
+  end
+
 end
