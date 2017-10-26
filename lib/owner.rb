ebf0c5fdd471261ac60eaa5c0f21623ac539e6a8
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-26 02:20:50 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,81 @@
 class Owner
-  # code goes here
+
+  attr_accessor :name, :pets
+  attr_reader :species
+
+  @@all = []
+  @@count = 0
+
+  def self.reset_all
+    @@count = 0
+  end
+
+  def initialize(name, species = "human")
+    @name = name
+    @species = species
+    @pets = {fishes: [], cats: [], dogs: []}
+    @@all << self
+    @@count += 1
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.all
+    @@all
+  end
+
+  def say_species
+    return "I am a #{self.species}."
+  end
+
+  def buy_fish(name)
+    fish = Fish.new(name)
+    self.pets[:fishes] << fish
+  end
+
+  def buy_cat(name)
+    cat = Cat.new(name)
+    self.pets[:cats] << cat
+  end
+
+  def buy_dog(name)
+    dog = Dog.new(name)
+    self.pets[:dogs] << dog
+  end
+
+  def walk_dogs
+    self.pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def play_with_cats
+    self.pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    self.pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    self.pets.each do |type, pets|
+      self.pets[type].each do |pets|
+        pets.mood = "nervous"
+      end
+      pets.clear
+    end
+  end
+
+  def list_pets
+    count_dogs = self.pets[:dogs].length
+    count_cats = self.pets[:cats].length
+    count_fishes = self.pets[:fishes].length
+    return "I have #{count_fishes} fish, #{count_dogs} dog(s), and #{count_cats} cat(s)."
+  end
 end
